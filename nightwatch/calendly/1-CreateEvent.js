37757bc68b93b7ee3cfe57c2f84edf0b7caf8583
Vinh Nguyen <53579621+davinhster@users.noreply.github.com>
2024-03-28 13:13:47 -0700

    2. Verify that you can create an event with buffer time

--- nightwatch/calendly/1-CreateEvent.js
+++ nightwatch/calendly/1-CreateEvent.js
@@ -1,6 +1,5 @@
 describe('Create Event with no customization', function() {
-    var event_name = "1-Vinh Nguyen's Meeting";
-    var event_name_selector = "1-Vinh Nguyen\\'s Meeting";
+    var event_name_selectors = [];
     before(function(browser) {
         browser
             .navigateTo('https://calendly.com/')
@@ -9,13 +8,19 @@ describe('Create Event with no customization', function() {
             .click("a[href='/login'] span:first-child") // click login button
             .waitForElementPresent("input[type='email']", 10000)
             .setValue("input[type='email']",process.env.USERNAME)  // enter username
+            .waitForElementPresent("button[type='submit']", 10000)
             .click("button[type='submit']") // click login
             .waitForElementPresent("input[type='password']", 10000)
             .setValue("input[type='password']",process.env.PASSWORD) // enter password
             .click("button[type='submit']") // click continue
+            .waitForElementPresent("div[class^='event-type-group-list-item user-item']",10000)
+            .expect.element("div[class='event-type-card-list']").to.be.not.present
     });
   
     it('Should create an event with no customization', function(browser) {
+        let event_name = "1-Vinh Nguyen's Meeting";
+        let event_name_selector = "1-Vinh Nguyen\\'s Meeting";
+        event_name_selectors.push(event_name_selector)
         browser
             .waitForElementPresent("a[href*='event_types/new']",10000)
             .execute(function() { // using this function since click() doesnt support * locators
@@ -35,14 +40,49 @@ describe('Create Event with no customization', function() {
             .assert.visible(`button[aria-label='${event_name_selector}']`) // verify event card is created
     });
 
-    after(function(browser) {
+    it('Should create an event with buffer time', function(browser) {
+        let event_name = "2-Vinh Nguyen's Meeting";
+        let event_name_selector = "2-Vinh Nguyen\\'s Meeting";
+        event_name_selectors.push(event_name_selector)
         browser
-            .click(`button[aria-label='${event_name_selector} settings']`) // click on gear icon
-            .click(`button[aria-label='${event_name_selector} settings'] + div button:nth-of-type(5)`) // click on delete
-            .click("div[role='dialog'] button:nth-of-type(2)") // click delete
-            .waitForElementNotPresent(`button[aria-label='${event_name_selector}']`, 10000)
-            .assert.not.elementPresent(`button[aria-label='${event_name_selector}']`) // verify event card has been deleted
-            .end()
+            .waitForElementPresent("a[href*='event_types/new']",10000)
+            .execute(function() { // using this function since click() doesnt support * locators
+                document.querySelector("a[href*='event_types/new']").click(); // create new event
+            })
+            .waitForElementPresent("tr[data-component='one-on-one'] button:first-child", 10000)
+            .click("tr[data-component='one-on-one'] button:first-child") // select one on one meeting
+            .waitForElementPresent("div[role='dialog'] button:nth-of-type(2)", 10000)
+            .click("div[role='dialog'] button:nth-of-type(2)") // select next
+            .waitForElementPresent("form input", 10000)
+            .setValue("form input#event-name-field",event_name) // name event
+            .click("div[data-testid='event-type-editor-footer'] button:nth-of-type(2)") // click continue
+            .waitForElementPresent("div[data-testid='event-type-editor'] button[aria-label='Close']", 10000)
+            .click("div[data-testid='event-type-editor'] button[aria-label='Close']") // close notification
+            .click("button[data-calendly-label='scheduling_settings_section']") // click scheduling settings
+            .click("div[name='buffer_time'] button") // click on buffer time
+            .click("div[aria-labelledby='before_buffer_label']") // select before buffer
+            .waitForElementPresent("div[aria-labelledby='before_buffer_label'] + div button:nth-of-type(4)",10000)
+            .click("div[aria-labelledby='before_buffer_label'] + div button:nth-of-type(4)") // select 15 minutes
+            .click("div[aria-labelledby='after_buffer_label']") // select after buffer
+            .waitForElementPresent("div[aria-labelledby='after_buffer_label']+ div button:nth-of-type(4)",10000)
+            .click("div[aria-labelledby='after_buffer_label']+ div button:nth-of-type(4)") // select 15 minutes
+            .click("div[data-testid='event-type-editor-footer'] button:nth-of-type(2)") // click save and close
+            .pause(2000) // pause for calendly to save event configuration
+            .waitForElementPresent("div[data-testid='event-type-editor'] div:first-child button",10000)
+            .click("div[data-testid='event-type-editor'] div:first-child button") // click done
+            .waitForElementPresent(`button[aria-label='${event_name_selector}']`, 10000)
+            .assert.visible(`button[aria-label='${event_name_selector}']`) // verify event card is created
+    });
+
+    after(function(browser) {
+        for (let event_name_selector of event_name_selectors) {
+            browser
+                .click(`button[aria-label='${event_name_selector} settings']`) // click on gear icon
+                .click(`button[aria-label='${event_name_selector} settings'] + div button:nth-of-type(5)`) // click on delete
+                .click("div[role='dialog'] button:nth-of-type(2)") // click delete
+                .waitForElementNotPresent(`button[aria-label='${event_name_selector}']`, 10000)
+                .assert.not.elementPresent(`button[aria-label='${event_name_selector}']`) // verify event card has been deleted
+        }
     });
 });
   
