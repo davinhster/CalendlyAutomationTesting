584661d8cb3f35004fad9795a83f6a4516a8fadc
Vinh Nguyen <53579621+davinhster@users.noreply.github.com>
2024-03-28 23:33:33 -0700

    8. Verify that an error displays when entering invalid custom duration

--- nightwatch/calendly/CreateEventErrorHandlingTestSuite.js
+++ nightwatch/calendly/CreateEventErrorHandlingTestSuite.js
@@ -52,7 +52,7 @@ describe('Create Event Error Handling Tests', function() {
             .assert.visible(`button[aria-label='${event_name_selector}']`) // verify event card is created
     });
 
-    it.skip('Should create an event with an invalid daily limit', function(browser) {
+    it.skip('Should create an event with invalid daily limit', function(browser) {
         let event_name = "7-Vinh Nguyen's Meeting";
         let event_name_selector = "7-Vinh Nguyen\\'s Meeting";
         event_name_selectors.push(event_name_selector)
@@ -79,6 +79,36 @@ describe('Create Event Error Handling Tests', function() {
             .setValue("div[name='daily_limit'] button + div input",10) // set daily limit to 10
             .click("div[data-testid='event-type-editor-footer'] button:nth-of-type(2)") // click save and close
             .pause(5000) // pause for calendly to save event configuration
+            .waitForElementPresent("div[data-testid='event-type-editor'] div:first-child button",10000, function() {
+                this.click("xpath","/html/body/div[6]/div[2]/div/div/div/div[1]/div/div/div[1]/div/button") // click done
+            })
+            .waitForElementPresent(`button[aria-label='${event_name_selector}']`, 10000)
+            .assert.visible(`button[aria-label='${event_name_selector}']`) // verify event card is created
+    });
+
+    it.skip('Should create an event with invalid custom duration', function(browser) {
+        let event_name = "8-Vinh Nguyen's Meeting";
+        let event_name_selector = "8-Vinh Nguyen\\'s Meeting";
+        event_name_selectors.push(event_name_selector)
+        browser
+            .waitForElementPresent("a[href*='event_types/new']",10000)
+            .execute(function() { // using this function since click() doesnt support * locators
+                document.querySelector("a[href*='event_types/new']").click(); // create new event
+            })
+            .waitForElementPresent("tr[data-component='one-on-one'] button:first-child", 10000)
+            .click("tr[data-component='one-on-one'] button:first-child") // select one on one meeting
+            .waitForElementPresent("div[role='dialog'] button:nth-of-type(2)", 10000)
+            .click("div[role='dialog'] button:nth-of-type(2)") // select next
+            .waitForElementPresent("form input", 10000)
+            .setValue("form input#event-name-field",event_name) // name event
+            .click("div[aria-labelledby='duration-label']")
+            .click("div[aria-labelledby='duration-label'] + div button:nth-of-type(5)")
+            .setValue("input[name='customEventDuration']",-1) // set custom duration to -1
+            .isVisible("xpath","//div[@data-testid='duration-form']/div/div/div/div[4]//div[contains(text(),'Error')]") // verify error message is shown
+            .setValue("input[name='customEventDuration']",1) // set custom duration to 1
+            .click("div[data-testid='event-type-editor-footer'] button:nth-of-type(2)") // click continue
+            .waitForElementPresent("div[data-testid='event-type-editor'] button[aria-label='Close']", 10000)
+            .click("div[data-testid='event-type-editor'] button[aria-label='Close']") // close notification
             .waitForElementPresent("div[data-testid='event-type-editor'] div:first-child button",10000, function() {
                 this.click("xpath","/html/body/div[6]/div[2]/div/div/div/div[1]/div/div/div[1]/div/button") // click done
             })
